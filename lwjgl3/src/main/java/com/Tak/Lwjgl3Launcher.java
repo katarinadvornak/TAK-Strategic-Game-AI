e6605a182f84fb6d276adca78fe61cf680d1cc53
wenxi <wx.huang@student.maastrichtuniversity.nl>
2024-09-11 18:59:30 +0200

    Ordering

--- lwjgl3/src/main/java/com/Tak/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/Tak/Lwjgl3Launcher.java
@@ -1,8 +1,7 @@
-package com.Tak.lwjgl3;
+package com.Tak;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.Tak.Main;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
--- lwjgl3/src/main/java/com/Tak/StartupHelper.java
+++ lwjgl3/src/main/java/com/Tak/StartupHelper.java
@@ -14,7 +14,7 @@
  */
 //Note, the above license and copyright applies to this file only.
 
-package com.Tak.lwjgl3;
+package com.Tak;
 
 import org.lwjgl.system.macosx.LibC;
 
